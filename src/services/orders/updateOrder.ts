ca72ce101256048f951bc70f6b2fc9b29767a145
Fernand MATIGNON <fernand.matignon@epitech.eu>
2021-10-30 09:09:59 +0200

    remove secret from code

--- src/services/orders/updateOrder.ts
+++ src/services/orders/updateOrder.ts
@@ -3,9 +3,11 @@ import axios from "axios";
 import { BASE_URL } from "services";
 
 export function setOrderPaid(id: number) {
-    return axios.post(`${BASE_URL}/orders/${id}/isPaid`, {}, { headers: { 'Authorization': 'eventsfoodsecret42' } })
+    var s = localStorage.getItem('secret');
+    return axios.post(`${BASE_URL}/orders/${id}/isPaid`, {}, { headers: { 'Authorization': s } })
 }
 
 export function setOrderDelivered(id: number) {
-    return axios.post(`${BASE_URL}/orders/${id}/isDelivered`, {}, { headers: { 'Authorization': 'eventsfoodsecret42' } })
+    var s = localStorage.getItem('secret');
+    return axios.post(`${BASE_URL}/orders/${id}/isDelivered`, {}, { headers: { 'Authorization': s } })
 }
