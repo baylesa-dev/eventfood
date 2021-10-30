2400e2ef71fe4ad26a275d951df532acae04a983
Fernand MATIGNON <fernand.matignon@epitech.eu>
2021-10-30 09:12:43 +0200

    ignore things

--- src/services/orders/updateOrder.ts
+++ src/services/orders/updateOrder.ts
@@ -3,11 +3,15 @@ import axios from "axios";
 import { BASE_URL } from "services";
 
 export function setOrderPaid(id: number) {
+    // @ts-ignore
     var s = localStorage.getItem('secret');
+    // @ts-ignore
     return axios.post(`${BASE_URL}/orders/${id}/isPaid`, {}, { headers: { 'Authorization': s } })
 }
 
 export function setOrderDelivered(id: number) {
+    // @ts-ignore
     var s = localStorage.getItem('secret');
+    // @ts-ignore
     return axios.post(`${BASE_URL}/orders/${id}/isDelivered`, {}, { headers: { 'Authorization': s } })
 }
--- src/store/useStore.tsx
+++ src/store/useStore.tsx
@@ -5,6 +5,7 @@ import { initializeStore, RootState } from 'store';
 export default function useStore(
     initialState?: RootState
 ): ReturnType<typeof initializeStore> {
+    // @ts-ignore
     const store = useMemo(() => initializeStore(initialState), [initialState]);
     return store;
 }
